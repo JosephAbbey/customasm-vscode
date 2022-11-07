0957b28747f998a0001dafc906315ea42314eaf4
Joseph Abbey <joseph@abbey1.org.uk>
2022-11-07 14:18:39 +0000

    add findExecutable function

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,35 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import path = require('path');
 
+/**
+ * @param {string} exe executable name (without extension if on Windows)
+ * @return {Promise<string|null>} executable path if found
+ * */
+async function findExecutable(exe) {
+    const envPath = process.env.PATH || "";
+    const envExt = process.env.PATHEXT || "";
+    const pathDirs = envPath
+        .replace(/["]+/g, "")
+        .split(path.delimiter)
+        .filter(Boolean);
+    const extensions = envExt.split(";");
+    const candidates = pathDirs.flatMap((d) =>
+        extensions.map((ext) => path.join(d, exe + ext))
+    );
+    try {
+        return await Promise.any(candidates.map(checkFileExists));
+    } catch (e) {
+        return null;
+    }
+
+    async function checkFileExists(filePath) {
+        if ((await fs.stat(filePath)).isFile()) {
+            return filePath;
+        }
+        throw new Error("Not a file");
+    }
+}
+
 export function activate(context: vscode.ExtensionContext) {
     let WASM: WebAssembly.WebAssemblyInstantiatedSource | null = null;
 
